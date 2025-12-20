d1e09eae5740cb1408c3c1e9389eac6ac6ac1b21
ZkAleeJoo <xxaleejooxx@gmail.com>
2025-12-20 11:50:56 -0500

    Update TaskManager.java

--- src/main/java/clp/zkaleejoo/managers/TaskManager.java
+++ src/main/java/clp/zkaleejoo/managers/TaskManager.java
@@ -20,45 +20,35 @@ public class TaskManager {
     }
 
     public void startTasks() {
-        // Tarea principal de limpieza
-        if (plugin.getMainConfigManager().getAutoClearEnabled()) {
-            taskId = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    double currentTps = Lag.getTPS();
-                    double minTps = plugin.getMainConfigManager().getAutoClearMinTps();
-
-                    if (currentTps >= minTps) {
-                        return; 
-                    }
-
-                    // Si el TPS es bajo, procedemos a limpiar
-                    entityClearer.clearEntities(false, null);
-                }
-            }.runTaskTimer(plugin,
-                    plugin.getMainConfigManager().getAutoClearInterval() * 20L,
-                    plugin.getMainConfigManager().getAutoClearInterval() * 20L
-            ).getTaskId();
-        }
-
-        // Tarea de advertencia
-        if (plugin.getMainConfigManager().getWarningEnabled()) {
-            int warningTime = plugin.getMainConfigManager().getWarningSecondsBefore();
-            long warningDelay = (plugin.getMainConfigManager().getAutoClearInterval() - warningTime) * 20L;
-
-            warningTaskId = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    String message = plugin.getMainConfigManager().getWarningMessage()
-                            .replace("{time}", String.valueOf(warningTime));
-                    Bukkit.broadcastMessage(MessageUtils.getColoredMessage(
-                            plugin.getMainConfigManager().getPrefix() + message));
-                }
-            }.runTaskTimer(plugin, warningDelay,
-                    plugin.getMainConfigManager().getAutoClearInterval() * 20L
-            ).getTaskId();
-        }
+    if (plugin.getMainConfigManager().getAutoClearEnabled()) {
+        taskId = new BukkitRunnable() {
+            @Override
+            public void run() {
+                entityClearer.clearEntities(false, null);
+            }
+        }.runTaskTimer(plugin,
+                plugin.getMainConfigManager().getAutoClearInterval() * 20L,
+                plugin.getMainConfigManager().getAutoClearInterval() * 20L
+        ).getTaskId();
     }
+
+    if (plugin.getMainConfigManager().getWarningEnabled()) {
+        int warningTime = plugin.getMainConfigManager().getWarningSecondsBefore();
+        long warningDelay = (plugin.getMainConfigManager().getAutoClearInterval() - warningTime) * 20L;
+
+        warningTaskId = new BukkitRunnable() {
+            @Override
+            public void run() {
+                String message = plugin.getMainConfigManager().getWarningMessage()
+                        .replace("{time}", String.valueOf(warningTime));
+                Bukkit.broadcastMessage(MessageUtils.getColoredMessage(
+                        plugin.getMainConfigManager().getPrefix() + message));
+            }
+        }.runTaskTimer(plugin, warningDelay,
+                plugin.getMainConfigManager().getAutoClearInterval() * 20L
+        ).getTaskId();
+    }
+}
 
     public void stopTasks() {
         if (taskId != -1) Bukkit.getScheduler().cancelTask(taskId);
